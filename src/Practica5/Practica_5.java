0b3baab4c86ab7680e7de4302eaff57b4cdd8877
saletG08 <salet.gutierrez.13594439@usalesiana.edu.bo>
2023-08-10 23:16:32 -0400

    Practica 5

--- src/Practica5/Practica_5.java
+++ src/Practica5/Practica_5.java
@@ -2,7 +2,7 @@ package Practica5;
 
 import java.util.Scanner;
 
-public class Main {
+public class Practica_5 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
